ba0107ca73b64f714c792a8b6a439cc4e0041614
matiasforge <matiasnicolasb.mj@gmail.com>
2023-12-05 01:12:06 -0300

    ventas

--- app_flask/__init__.py
+++ app_flask/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 import re
 
 app = Flask(__name__)
-app.secret_key = "caracolito"
+app.secret_key = "bd_artesanos"
 
 BASE_DATOS = "bd_artesanos"
 EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from app_flask.controladores import controlador_usuarios, controlador_tienda
+from app_flask.controladores import controlador_usuarios, controlador_tienda, controlador_ventas
 from app_flask import app
 
 if __name__ == "__main__":
