7d00bb9aedce5c78a8594b4eccbf86849f624382
AParAPares <amandapares87@gmail.com>
2023-12-05 17:12:53 -0300

    arreglando datos

--- app_flask/modelos/modelo_tienda.py
+++ app_flask/modelos/modelo_tienda.py
@@ -10,8 +10,8 @@ class Tienda:
 class productos:
     def __init__(self, datos):
         self.id = datos["id"]
-        self.id = datos["id_venta"]
-        self.id = datos["id_categoria"]
+        self.id_venta = datos["id_venta"]
+        self.id_categoria = datos["id_categoria"]
         self.nombre = datos["nombre"]
         self.descripcion = datos["descripcion"]
         self.precio = datos["precio"]
@@ -23,7 +23,7 @@ class productos:
 class categorias:
     def __init__(self, datos):
         self.id = datos["id"]
-        self.id = datos["id_producto"]
+        self.id_producto = datos["id_producto"]
         self.nommbre_categoria = datos["nombre_categoria"]
         self.fecha_creacion = ["fecha_creacion"]
         self.fecha_actualizacion = ["fecha_actualizacion"]
@@ -32,8 +32,8 @@ class categorias:
 class carritocompras:
     def __init__(self, datos):
         self.id = datos["id"]
-        self.id = datos["id_usuario"]
-        self.id = datos["id_producto"]
+        self.id_usuario = datos["id_usuario"]
+        self.id_producto = datos["id_producto"]
         self.cantidad = datos["cantidad"]
         self.precio = datos["precio"]
         self.fecha_creacion = ["fecha_creacion"]
@@ -42,8 +42,10 @@ class carritocompras:
 class comentarios:
     def __init__(self, datos):
         self.id = datos["id"]
-        self.id = datos["id_tienda"]
-        self.id = datos["id_producto"]
+        self.id_tienda = datos["id_tienda"]
+        self.id_producto = datos["id_producto"]
         self.comentario = datos["comentario"]
         self.fecha_creacion = ["fecha_creacion"]
         self.fecha_actualizacion = ["fecha_actualizacion"]
+
+
